edbac3bcd3357d62fbb61d1e7644099be9325377
Давид <bekoevdawid@yandex.ru>
2024-07-10 23:37:43 +0300

    8 4/5

---
+++ ImageFeed/ImageFeed/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Давид Бекоев on 10.07.2024.
+//
+
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell" 
+}
