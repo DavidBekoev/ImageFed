0258f9147198c39deb405ffd1339274ad6ec94a1
Давид <bekoevdawid@yandex.ru>
2024-07-18 19:21:20 +0300

    present view contr

--- ImageFeed/ImageFeed/SingleImageViewController.swift
+++ ImageFeed/ImageFeed/SingleImageViewController.swift
@@ -7,6 +7,21 @@
 
 import UIKit
  final class SingleImageViewController: UIViewController {
-    @IBOutlet var imageView: UIImageView!
+   @IBOutlet var imageView: UIImageView!
+     var image: UIImage?  {
+         didSet {
+             guard isViewLoaded else { return } // 1
+             imageView.image = image // 2
+         }
+     }
+     @IBAction private func didTapBackButton() {
+            dismiss(animated: true, completion: nil)
+        }
+    
+     
+     override func viewDidLoad() {
+             super.viewDidLoad()
+             imageView.image = image
+         }
     
 }
