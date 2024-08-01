17d3071689397de70ba02e1f599bd950f398def4
Давид <bekoevdawid@yandex.ru>
2024-08-02 00:38:34 +0300

    web view

---
+++ ImageFeed/ImageFeed/Constans.swift
@@ -0,0 +1,17 @@
+//
+//  Constans.swift
+//  ImageFeed
+//
+//  Created by Давид Бекоев on 27.07.2024.
+//
+
+import Foundation
+
+
+    let accessKey = "Ut8DhbYDqB3IYd6F4ga5POG3n-GGbfe9JRuy_fexFcQ"
+    let secretKey = "e0KEcCLBvpNgA-PoxrJEgd59FMe7UDJpdhsR6-eLX5A"
+    let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+   let accessScope = "public+read_user+write_likes"
+    let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    
+
